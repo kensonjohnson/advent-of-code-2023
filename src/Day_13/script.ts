7b99af22bda09ad68094838937855d37b1c1f34e
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-12-13 19:24:15 -0600

    Finish first task

--- src/Day_13/script.ts
+++ src/Day_13/script.ts
@@ -103,7 +103,136 @@ const testData = `#.##..##.
 #....#..#`;
 
 function firstTask(data: string) {
-  console.log("Day 13");
+  const sections = data.split("\n\n").map((section) => section.split("\n"));
+  let count = 0;
+  for (const section of sections) {
+    const horizontal = findHorizonalValue(section) * 100;
+    const vertical = findVerticalValue(section);
+    count += horizontal + vertical;
+  }
+  return count;
 }
 
-console.log(firstTask(testData));
+function findVerticalValue(section: string[]) {
+  for (let index = 0; index < section[0].length - 1; index++) {
+    // Add 1 because of the 0 index
+    if (isValidVertical(section, index)) return index + 1;
+  }
+  return 0;
+}
+
+function isValidVertical(section: string[], index: number) {
+  let valid = true;
+  let leftIndex = index;
+  let rightIndex = index + 1;
+  while (leftIndex >= 0 && rightIndex < section[0].length) {
+    // Iterate down all of the rows to create the columns
+    let leftColumn = section.map((row) => row[leftIndex]).join("");
+    let rightColumn = section.map((row) => row[rightIndex]).join("");
+    if (leftColumn !== rightColumn) {
+      valid = false;
+      break;
+    }
+    leftIndex--;
+    rightIndex++;
+  }
+  return valid;
+}
+
+function findHorizonalValue(section: string[]) {
+  for (let index = 0; index < section.length - 1; index++) {
+    // Add 1 because of the 0 index
+    if (isValidHorizontal(section, index)) return index + 1;
+  }
+  return 0;
+}
+
+function isValidHorizontal(section: string[], index: number) {
+  let valid = true;
+  let topIndex = index;
+  let bottomIndex = index + 1;
+  while (topIndex >= 0 && bottomIndex < section.length) {
+    let topRow = section[topIndex];
+    let bottomRow = section[bottomIndex];
+    if (topRow !== bottomRow) {
+      valid = false;
+      break;
+    }
+    topIndex--;
+    bottomIndex++;
+  }
+  return valid;
+}
+
+// console.log(firstTask(data));
+
+/* 
+You resume walking through the valley of mirrors and - SMACK! - run directly into one. 
+Hopefully nobody was watching, because that must have been pretty embarrassing.
+
+Upon closer inspection, you discover that every mirror has exactly one smudge: exactly 
+one . or # should be the opposite type.
+
+In each pattern, you'll need to locate and fix the smudge that causes a different 
+reflection line to be valid. (The old reflection line won't necessarily continue 
+being valid after the smudge is fixed.)
+
+Here's the above example again:
+
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+The first pattern's smudge is in the top-left corner. If the top-left # were 
+instead ., it would have a different, horizontal line of reflection:
+
+1 ..##..##. 1
+2 ..#.##.#. 2
+3v##......#v3
+4^##......#^4
+5 ..#.##.#. 5
+6 ..##..##. 6
+7 #.#.##.#. 7
+With the smudge in the top-left corner repaired, a new horizontal line of 
+reflection between rows 3 and 4 now exists. Row 7 has no corresponding 
+reflected row and can be ignored, but every other row matches exactly: 
+row 1 matches row 6, row 2 matches row 5, and row 3 matches row 4.
+
+In the second pattern, the smudge can be fixed by changing the fifth symbol 
+on row 2 from . to #:
+
+1v#...##..#v1
+2^#...##..#^2
+3 ..##..### 3
+4 #####.##. 4
+5 #####.##. 5
+6 ..##..### 6
+7 #....#..# 7
+Now, the pattern has a different horizontal line of reflection between rows 1 
+and 2.
+
+Summarize your notes as before, but instead use the new different reflection lines. 
+In this example, the first pattern's new horizontal line has 3 rows above it and 
+the second pattern's new horizontal line has 1 row above it, summarizing to the 
+value 400.
+
+In each pattern, fix the smudge and find the different line of reflection. What 
+number do you get after summarizing the new reflection line in each pattern in your notes?
+*/
+
+function secondTask(data: string) {
+  // Code
+}
+
+console.log(secondTask(testData));
